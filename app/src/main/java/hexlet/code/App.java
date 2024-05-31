2d83ab9bd4db17ad2b47bde419239b304b8f32af
maximkoo <svazarm@yandex.ru>
2024-05-31 22:28:51 +0300

    Some changes in App.java!

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,2 +1,8 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
 }
