138c235c340435f5846fa9545955e8e9a8665f20
Maxim Kopachevsky <kopachevskiima@sovcombank.ru>
2024-11-23 20:58:37 +0300

    Engine works fine with greet, even and calc

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -16,84 +16,79 @@ public class App {
         System.out.println();
         //sc.close();
         if (choice.equals("1")) {
-            String username = greet();
+            //String username = greet();
+            Engine.greet();
         } else if (choice.equals("2")) {
-            even();
+            //even();
+            Engine.flow("even");
         } else if (choice.equals("3")) {
-            calc();
+            //calc();
+            Engine.flow("calc");
         }
     }
 
+//    public static void even() {
+//        String username = greet();
+//        String correctAnswer;
+//        int cnt = 0;
+//        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+//        while (cnt < 3) {
+//            int num = (int) (100 * Math.random());
+//            if (num % 2 == 0) {
+//                correctAnswer = "yes";
+//            } else {
+//                correctAnswer = "no";
+//            }
+//            System.out.println("Question: " + num);
+//            System.out.print("Your answer: ");
+//            Scanner sc = new Scanner(System.in);
+//            String answer = sc.next();
+//            if (answer.equals(correctAnswer)) {
+//                cnt++;
+//                System.out.println("Correct!");
+//            } else {
+//                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
+//                System.out.println("Let's try again, " + username);
+//                return;
+//            }
+//        }
+//    }
+//
+//    public static void calc() {
+//        String username = greet();
+//        String correctAnswer;
+//        System.out.println("What is the result of the expression?");
+//        int cnt = 0;
+//        while (cnt < 3) {
+//            int n1 = 1+(int) (20 * Math.random());
+//            int n2 = 1+(int) (20 * Math.random());
+//            //int sgnN = (int) (4 * Math.random());
+//            String sgn = switch ((int) (3 * Math.random())) {
+//                case 0 -> "+";
+//                case 1 -> "-";
+//                case 2 -> "*";
+//                default -> "+";
+//            };
+//            correctAnswer = switch (sgn){
+//                case "+" -> Integer.valueOf(n1+n2).toString();
+//                case "-" -> Integer.valueOf(n1-n2).toString();
+//                case "*" -> Integer.valueOf(n1*n2).toString();
+//                default -> "0";
+//            };
+//            System.out.printf("Question: %d %s %d%n",n1, sgn, n2);
+//            Scanner sc = new Scanner(System.in);
+//            String answer = sc.next();
+//            if (answer.equals(Integer.valueOf(correctAnswer).toString())) {
+//                cnt++;
+//                System.out.println("Correct!");
+//            } else {
+//                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
+//                System.out.println("Let's try again, " + username);
+//                return;
+//            }
+//        }
+//    }
 
-    public static String greet() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String username = sc.next();
-        System.out.println("Hello, " + username);
-        return username;
-    }
 
-    public static void even() {
-        String username = greet();
-        String correctAnswer;
-        int cnt = 0;
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        while (cnt < 3) {
-            int num = (int) (100 * Math.random());
-            if (num % 2 == 0) {
-                correctAnswer = "yes";
-            } else {
-                correctAnswer = "no";
-            }
-            System.out.println("Question: " + num);
-            System.out.print("Your answer: ");
-            Scanner sc = new Scanner(System.in);
-            String answer = sc.next();
-            if (answer.equals(correctAnswer)) {
-                cnt++;
-                System.out.println("Correct!");
-            } else {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
-                System.out.println("Let's try again, " + username);
-                return;
-            }
-        }
-    }
-
-    public static void calc() {
-        String username = greet();
-        String correctAnswer;
-        System.out.println("What is the result of the expression?");
-        int cnt = 0;
-        while (cnt < 3) {
-            int n1 = 1+(int) (20 * Math.random());
-            int n2 = 1+(int) (20 * Math.random());
-            //int sgnN = (int) (4 * Math.random());
-            String sgn = switch ((int) (3 * Math.random())) {
-                case 0 -> "+";
-                case 1 -> "-";
-                case 2 -> "*";
-                default -> "+";
-            };
-            correctAnswer = switch (sgn){
-                case "+" -> Integer.valueOf(n1+n2).toString();
-                case "-" -> Integer.valueOf(n1-n2).toString();
-                case "*" -> Integer.valueOf(n1*n2).toString();
-                default -> "0";
-            };
-            System.out.printf("Question: %d %s %d%n",n1, sgn, n2);
-            Scanner sc = new Scanner(System.in);
-            String answer = sc.next();
-            if (answer.equals(Integer.valueOf(correctAnswer).toString())) {
-                cnt++;
-                System.out.println("Correct!");
-            } else {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
-                System.out.println("Let's try again, " + username);
-                return;
-            }
-        }
-    }
 }
 
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,7 +1,77 @@
 package hexlet.code;
 
-public class Engine {
-    public static void flow(){
+import java.util.Scanner;
 
+public class Engine {
+    public static void flow(String gameType) {
+        String username = greet();
+        int cnt = 0;
+        String answer;
+        String correctAnswer;
+        while (cnt < 3) {
+            Scanner sc = new Scanner(System.in);
+            correctAnswer = switch (gameType) {
+                case "even" -> even(username);
+                case "calc" -> calc(username);
+                default -> "";
+            };
+            answer = sc.next();
+            if (answer.equals(correctAnswer)) {
+                cnt++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
+                System.out.println("Let's try again, " + username);
+                return;
+            }
+        }
+    }
+
+
+    public static String greet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String username = sc.next();
+        System.out.println("Hello, " + username);
+        return username;
+    }
+
+    public static String even(String username) {
+        //String username = greet();
+        String correctAnswer;
+        //int cnt = 0;
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        //while (cnt < 3) {
+        int num = (int) (100 * Math.random());
+        if (num % 2 == 0) {
+            correctAnswer = "yes";
+        } else {
+            correctAnswer = "no";
+        }
+        System.out.println("Question: " + num);
+        return correctAnswer;
+    }
+
+    public static String calc(String username) {
+        String correctAnswer;
+        System.out.println("What is the result of the expression?");
+        int cnt = 0;
+        int n1 = 1 + (int) (20 * Math.random());
+        int n2 = 1 + (int) (20 * Math.random());
+        String sgn = switch ((int) (3 * Math.random())) {
+            case 0 -> "+";
+            case 1 -> "-";
+            case 2 -> "*";
+            default -> "+";
+        };
+        correctAnswer = switch (sgn) {
+            case "+" -> Integer.valueOf(n1 + n2).toString();
+            case "-" -> Integer.valueOf(n1 - n2).toString();
+            case "*" -> Integer.valueOf(n1 * n2).toString();
+            default -> "0";
+        };
+        System.out.printf("Question: %d %s %d%n", n1, sgn, n2);
+        return correctAnswer;
     }
 }
